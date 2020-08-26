a5be01ba504369b01742fa7a90c2597b6d2ff0dc
João Henrique <joaohenrique.p.almeida@gmail.com>
2020-08-26 12:46:24 -0300

    Change server

--- server.js
+++ server.js
@@ -2,21 +2,25 @@
 
 const express = require('express');
 const url = require('url');
-
+const socketio = require('socket.io');
 // const WebSocket = require('ws').Server;
 
 const PORT = process.env.PORT || 9000;
 const INDEX = '/index.html';
 
-const server = express()
+const app = express()
     .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // const wss = new WebSocket({ server });
 
-const io = require('socket.io')(server, {
+console.log("SNIP WEBSOCKET EDIT FILE SERVER UP!!!");
+// const wss = new WebSocket({ server: app });
+
+const io = socketio.listen(app, {
     transports: ['websocket'],
     pingInterval: 0,
+    path: '/websocket',
     pingTimeout: 50000,
     perMessageDeflate: false,
     handlePreflightRequest: (req, res) => {
@@ -28,9 +32,8 @@ const io = require('socket.io')(server, {
         res.writeHead(200, headers);
         res.end();
     }
-})
+});
 
-console.log("SNIP WEBSOCKET EDIT FILE SERVER UP!!!");
 var CHANNELS = [];
 var USERS = [];
 
@@ -49,27 +52,26 @@ var getMessage = function (data, text, user, users, status) {
 
 var getChannels = function (data) {
     return CHANNELS[data.collection][data.thing][0];
-}
+};
 
 var setInfoChannels = function (data, status, user) {
     CHANNELS[data.collection][data.thing][0]["status"] = status;
     CHANNELS[data.collection][data.thing][0]["id_edit"] = user.id;
     CHANNELS[data.collection][data.thing][0]["user_edit"] = user;
-}
-
+};
 
 io.on('connection', (socket) => {
-
+    console.log("Connected", socket.id);
     socket.on('open-edit', function (info) {
 
         console.log('OPEN EDIT___________________________________');
         var data = JSON.parse(info);
-        var room = data.collection + data.thing
+        var room = data.collection + data.thing;
         socket.join(room);
         USERS.push({ data, id: socket.id });
 
         //Connectados no channel
-        var elements = Object.keys(io.sockets.adapter.rooms[room].sockets)
+        var elements = Object.keys(io.sockets.adapter.rooms[room].sockets);
 
         if (Object.keys(CHANNELS).indexOf(data.collection) === -1) {
             CHANNELS[data.collection] = [];
@@ -84,10 +86,11 @@ io.on('connection', (socket) => {
         //  Procurar uma melhor forma de realizar esse filter - provavelmente listar os usuário de um canal e selecionar pelo socket_id
         var users = USERS.filter(user => (user.data.collection === data.collection && user.data.thing === data.thing));
 
+        var user;
         if (channel.user_edit === '') {
-            var user = '';
+            user = '';
         } else {
-            var user = channel.user_edit
+            user = channel.user_edit;
         }
 
         var text = (users.length > 1) ? "Outras pessoas estão visualizando o mesmo arquivo" : "";
@@ -109,7 +112,7 @@ io.on('connection', (socket) => {
 
         console.log("unarchived-edit________________");
         var info = JSON.parse(data);
-        var room = info.collection + info.thing
+        var room = info.collection + info.thing;
         var users = USERS.filter(user => (user.data.collection === info.collection && user.data.thing === info.thing));
         var user = users.filter(user => (user.id === socket.id));
 
@@ -127,7 +130,7 @@ io.on('connection', (socket) => {
     socket.on('archived-edit', function (data) {
         console.log("archived-edit");
         var info = JSON.parse(data);
-        var room = info.collection + info.thing
+        var room = info.collection + info.thing;
         var users = USERS.filter(user => (user.data.collection === info.collection && user.data.thing === info.thing));
         var user = users.filter(user => (user.id === socket.id))[0];
 
@@ -143,7 +146,7 @@ io.on('connection', (socket) => {
     socket.on('save-edit', function (data) {
         console.log("Save-edit____________");
         var info = JSON.parse(data);
-        var room = info.collection + info.thing
+        var room = info.collection + info.thing;
         var users = USERS.filter(user => (user.data.collection === info.collection && user.data.thing === info.thing));
         var user = users.filter(user => (user.id === socket.id))[0];
 
@@ -159,15 +162,14 @@ io.on('connection', (socket) => {
             if (channel.id_edit != element) {
                 io.to(element).emit('message', msg);
             }
-        })
-
+        });
         // socket.to(room).emit('message', msg);
     });
 
     socket.on('file-edit', function (data) {
         console.log("File Edit__________________________________________________________________________");
         var info = JSON.parse(data);
-        var room = info.collection + info.thing
+        var room = info.collection + info.thing;
         var users = USERS.filter(user => (user.data.collection === info.collection && user.data.thing === info.thing));
         var user = users.filter(user => user.id === socket.id)[0];
 
@@ -184,71 +186,76 @@ io.on('connection', (socket) => {
             if (channel.id_edit != element) {
                 io.to(element).emit('message', msg);
             }
-        })
+        });
         // socket.to(room).emit('message', msg);
     });
 
     socket.on('close-connection', function (info) {
-
         console.log('close Connection________________________________________________--');
-        var data = JSON.parse(info);
-        var room = data.collection + data.thing
-        socket.leave(room);
-        var index = USERS.findIndex(user => user.id === socket.id);
-        var user_off = USERS.filter(user => user.id === socket.id)[0];
-        USERS.splice(index, 1);
-        var users = USERS.filter(user => (user.data.collection === data.collection && user.data.thing === data.thing));
+        console.log(info);
+        if (info != null) {
+            var data = JSON.parse(info);
+            var room = data.collection + data.thing;
+            socket.leave(room);
+            var index = USERS.findIndex(user => user.id === socket.id);
+            var user_off = USERS.filter(user => user.id === socket.id)[0];
+            USERS.splice(index, 1);
+            var users = USERS.filter(user => (user.data.collection === data.collection && user.data.thing === data.thing));
 
-        var check = getChannels(user_off.data)
-
-        if (check.id_edit === socket.id) {
-            if (check.status != 'archived') {
-                check.status = 'alert';
-            }
-        }
-
-        var info_channel = getChannels(data);
-
-        var user = info_channel.user_edit;
-        var status = info_channel.status;
-        var text = "";
-        var msg = getMessage(data, text, user, users, status);
-
-        if (info_channel.status !== 'block') {
-            io.in(room).emit('message', msg);
-        } else {
-            var clients = Object.keys(io.sockets.adapter.rooms[room].sockets);
-            clients.forEach(element => {
-                if (info_channel.id_edit != element) {
-                    io.to(element).emit('message', msg);
-                }
-            });
-        }
-    });
-
-    socket.on('disconnect', function () {
-        console.log('client disconnect...______________________________________________');
-        var room = data.collection + data.thing
-        var index = USERS.findIndex(user => user.id === socket.id);
-        var user_off = USERS.filter(user => user.id === socket.id)[0];
-        USERS.splice(index, 1);
-
-        if (user_off != undefined) {
             var check = getChannels(user_off.data);
+
             if (check.id_edit === socket.id) {
                 if (check.status != 'archived') {
                     check.status = 'alert';
                 }
             }
 
-            var users = USERS.filter(user => (user.data.collection === user_off.data.collection && user.data.thing === user_off.data.thing));
-            var user = check.user_edit;
-            var data = { collection: user_off.data.collection, thing: user_off.data.thing };
-            var status = check.status;
-            var text = '';
+            var info_channel = getChannels(data);
+
+            var user = info_channel.user_edit;
+            var status = info_channel.status;
+            var text = "";
             var msg = getMessage(data, text, user, users, status);
 
-            io.in(room).emit('message', msg);
+            if (info_channel.status !== 'block') {
+                io.in(room).emit('message', msg);
+            } else {
+                var clients = Object.keys(io.sockets.adapter.rooms[room].sockets);
+                clients.forEach(element => {
+                    if (info_channel.id_edit != element) {
+                        io.to(element).emit('message', msg);
+                    }
+                });
+            }
+        }
+    });
+
+    socket.on('disconnect', function () {
+
+        if (USERS.findIndex(user => user.id === socket.id) != -1) {
+            console.log('client disconnect...______________________________________________');
+            var user_off = USERS.filter(user => user.id === socket.id)[0];
+            var room = user_off.collection + user_off.thing;
+            var index = USERS.findIndex(user => user.id === socket.id);
+            USERS.splice(index, 1);
+
+            if (user_off != undefined) {
+                var check = getChannels(user_off.data);
+                if (check.id_edit === socket.id) {
+                    if (check.status != 'archived') {
+                        check.status = 'alert';
+                    }
+                }
+
+                var users = USERS.filter(user => (user.data.collection === user_off.data.collection && user.data.thing === user_off.data.thing));
+                var user = check.user_edit;
+                var data = { collection: user_off.data.collection, thing: user_off.data.thing };
+                var status = check.status;
+                var text = '';
+                var msg = getMessage(data, text, user, users, status);
+
+                io.in(room).emit('message', msg);
+            }
         }
 
     });
@@ -259,11 +266,11 @@ io.on('connection', (socket) => {
     });
 });
 
+// AQUI COMEÇA O NOVO
 
-//// AQUI COMEÇA O NOVO
-
-// initialization
+// // initialization
 // var CHANNELS = [];
+// var USERS = [];
 
 // wss.broadcastChannel = function broadcastChannel(channel, data) {
 
@@ -400,15 +407,23 @@ io.on('connection', (socket) => {
 
 // wss.on('connection', (ws, req) => {
 
-//     ws.on('test', function test() { 
-//         console.log("sfvpviernjviowrjv-mwróvjmeropbjepobjeopbjeopbkqepobjpotedjk");
-//     });
-
 //     ws.on('message', function incoming(data) {
 //         console.log(data);
 //         data = JSON.parse(data);
 //         switch (data.message) {
 //             case 'open-edit':
+
+//                 // USERS.push({ data, connection: ws })
+
+//                 // if (Object.keys(CHANNELS).indexOf(data.collection) === -1) {
+//                 //     CHANNELS[data.collection] = [];
+//                 //     CHANNELS[data.collection][data.thing] = [{ "status": "alert", "user_edit": "", "id_edit": "" }];
+//                 // } else {
+//                 //     if (Object.keys(CHANNELS[data.collection]).indexOf(data.thing) === -1) {
+//                 //         CHANNELS[data.collection][data.thing] = [{ "status": "alert", "user_edit": "", "id_edit": "" }];
+//                 //     }
+//                 // }
+
 //                 if (Object.keys(CHANNELS).indexOf(data.collection) === -1) {
 //                     console.log('Não Tinha');
 //                     CHANNELS[data.collection] = [];
@@ -429,6 +444,7 @@ io.on('connection', (socket) => {
 //                     }
 //                 }
 //                 break;
+
 //             case 'unarchive-edit':
 //                 console.log("Unarchived");
 //                 CHANNELS[data.collection][data.thing]["status"] = "alert";
